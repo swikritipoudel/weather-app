b424ef6ad908b8f9d81bfe1f1ab77f531942232f
Swikriti Poudel <poudelswikriti76@gmail.com>
2025-06-15 20:41:08 +0545

    Added functionality to display the weather data.

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener("submit", async event=>{
             displayWeatherInfo(weatherData)
         }
 
-        catch(error){
+        catch(error){ 
             console.error(error)
             displayError(error)
         }
@@ -37,7 +37,11 @@ async function getWeatherData(city){
 
     const response = await fetch(apiUrl)
 
-    console.log(response)
+   if(!response.ok){
+    throw new Error("Could not fetch weather data")
+   }
+
+   return await response.json()
 
 }
 
@@ -48,7 +52,67 @@ async function getWeatherData(city){
 
 
 function displayWeatherInfo(data){
+    const {name:city, 
+          main: {temp, humidity}, 
+          weather:[{description, id}]} = data
 
+   card.textContent = ""
+   card.style.display = "flex" 
+   
+   const cityDisplay = document.createElement("h1")
+   const tempDisplay = document.createElement("p")
+   const humidityDisplay = document.createElement("p")
+   const descDisplay = document.createElement("p")
+   const weatherEmoji = document.createElement("p")
+
+   cityDisplay.textContent = city
+   tempDisplay.textContent = `${(temp- 273.15) .toFixed(1)}°C`
+   humidityDisplay.textContent = `Humidity: ${humidity}%`
+   descDisplay.textContent = description
+   weatherEmoji.textContent = getWeatherEmoji(id)
+
+   cityDisplay.classList.add("cityDisplay")
+   tempDisplay.classList.add("tempDisplay")
+   humidityDisplay.classList.add("humidityDisplay")
+   descDisplay.classList.add("descDisplay")
+   weatherEmoji.classList.add("weatherEmoji")
+
+   card.appendChild(cityDisplay)
+   card.appendChild(tempDisplay)
+   card.appendChild(humidityDisplay)
+   card.appendChild(descDisplay)
+   card.appendChild(weatherEmoji)
+
+}
+
+function getWeatherEmoji(weatherId){
+
+    switch(true){
+        case(weatherId>=200 && weatherId<300):
+        return "⛈️"
+
+        case(weatherId>=300 && weatherId<400):
+        return "🌧️"
+
+        case(weatherId>=500 && weatherId<600):
+        return "🌧️"
+
+        case(weatherId>=600 && weatherId<700):
+        return "☃️"
+
+        case(weatherId>=700 && weatherId<800):
+        return "🌫️"
+
+        case(weatherId===800):
+        return "☀️"
+
+        case(weatherId>=801 && weatherId<810):
+        return "☁️"
+
+        default:
+            return"?"
+
+    }
 }
 
 
